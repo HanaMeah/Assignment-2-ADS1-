ed01c23b5ed4c4cfa93fac5c20414e0f5ff91a2e
HanaMeah <148878773+HanaMeah@users.noreply.github.com>
2023-12-12 00:03:57 +0000

    Add files via upload
    upload 2

--- ADS1 Coursework 2 .py
+++ ADS1 Coursework 2 .py
@@ -16,6 +16,15 @@ import pandas as pd
 
 # functions
 
+# def readmydata(df):
+#     """
+
+# This is a function to read my data files
+
+    
+# """
+#     pd.read_csv(df)
+#     return
 
 # main program
 
@@ -24,11 +33,18 @@ df_GDP_per_country = pd.read_csv("GDPpercentage.csv")
 df_GDP_per_country.dropna()
 
 df_GDP_per_country.drop(
-    df_GDP_per_country.columns[[0, 2, 3]], axis=1, inplace=True)
+    df_GDP_per_country.columns[[0, 2, 3, 4, 5, -1]], axis=1, inplace=True)
 
-n = df_GDP_per_country.iloc[0:211]
+#make a subset with the countries I want to look at in GDPpercentage
+GDP_df = df_GDP_per_country.iloc[1:11]
 
-print(n.head(211))
-#print(df_GDP_per_country.head(160))
+print(GDP_df.head(11))
+
+#second data set I am comparing
+#Access to Clean Fuels and Technologies for Cooking and Rural percentage
+cleanfuel_df = pd.read_csv("CleanFuelsTechpercentage.csv")
+print(cleanfuel_df.head(11))
+
+
 
 
