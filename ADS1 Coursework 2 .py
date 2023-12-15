f7cb7d3ef12c1f1f0dbd1cc9cbb5a435b093a3b7
HanaMeah <148878773+HanaMeah@users.noreply.github.com>
2023-12-15 14:16:05 +0000

    Add files via upload
    15/12/2023

--- ADS1 Coursework 2 .py
+++ ADS1 Coursework 2 .py
@@ -10,43 +10,49 @@ Hana Meah
 
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import kurtosis
+from scipy.stats import skew
 
 # functions
 
-# def readmydata(df):
-#     """
 
-# This is a function to read my data files
+def GDPpercountry(datafilename):
+    """
 
+    Function to read my data csv file which also gives the output of my 2 
+    transposed data frames
+
+    """
+    df_orig = pd.read_csv(datafilename)   
+    columns = df_orig.columns[1:]
+ #   print(columns)
+    df_orig[columns] = df_orig[columns].apply(pd.to_numeric)
     
-# """
-#     pd.read_csv(df)
-#     return
+    
+ #   print(df_orig)
+ #   print(df_orig.dtypes)
+
+    # Transposing my dataframe
+    df_transposed = df_orig.transpose()
+    df_transposed.columns = df_transposed.iloc[0]
+    df_transposed = df_transposed.iloc[1:]
+    df_transposed = df_transposed.apply(pd.to_numeric)
+ #  print(df_transposed.columns)
+    
+ #  print(df_transposed.dtypes)
+    
+    
+    return df_transposed, df_orig
 
 # main program
+df_years, df_countries = GDPpercountry("GDPpercentage.csv")
 
-df_GDP_per_country = pd.read_csv("GDPpercentage.csv")
 
-df_GDP_per_country.dropna()
-
-df_GDP_per_country.drop(
-    df_GDP_per_country.columns[[0, 2, 3, 4, 5, -1]], axis=1, inplace=True)
-
-#make a subset with the countries I want to look at in GDPpercentage
-GDP_df = df_GDP_per_country.iloc[1:11]
-
-GDP_df.transpose()
-
-print(GDP_df.head(11))
-
-#second data set I am comparing
-#IMprtzs percentage
+# second data set I am comparing
+# IMprtzs percentage
 imports_df = pd.read_csv("ImportsofGoodsiNservices.csv")
-print(imports_df.head(11))
-
-
-
-
+#print(imports_df.head(11))
